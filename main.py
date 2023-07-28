7dd83d54690d0da8bf752819e0eff22311517c19
BrickWallLax <69017601+BrickWallLax@users.noreply.github.com>
2023-07-28 10:30:06 -0400

    Final

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 import bs4
 import urllib.parse as parse
+from urlextract import URLExtract
 
-final = {'name': [], 'webpage': [], 'birthplace': []}
+extractor = URLExtract()
+final = {'name': [], 'webpage': [], 'image': [], 'birthplace': []}
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
@@ -24,16 +26,21 @@ def get_info(first_name, last_name):
 
     for player in players:
         player_info = bs4.BeautifulSoup(player.decode(), 'lxml')
+
         player_name = player_info.find('div', attrs={'class': 'name'}).string
         final.setdefault('name', []).append(player_name)
 
         player_web = player_info.find('a').get('href')
-        player_web = parse.parse_qsl(player_web)
-        final.setdefault('webpage', []).append(player_web[1][1])
+        player_id = parse.parse_qsl(player_web)
+        final.setdefault('id', []).append(player_id[1][1])
+
+        player_img = player_info.find('span', attrs={'class': 'db-gallery__thumbnail-image'}).get('style')
+        urls = extractor.find_urls(player_img)
+        final.setdefault('image', []).append(urls[0])
 
         player_birth = player_info.find('div', attrs={'class': 'db-gallery__item-subtext'}).string
         final.setdefault('birthplace', []).append(player_birth)
 
 
-get_info('robert', 'kaplan')
-print(final['name'], final['webpage'], final['birthplace'])
+get_info('darin', 'kaplan')
+print(final['name'], final['id'], final['image'], final['birthplace'])
